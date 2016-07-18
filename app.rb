5a0b520cc49b447527e26ffb35480750116d17a2
Ana <pejagomss@gmail.com>
2016-07-18 00:07:00 +0000

    new post fixed

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class HangpersonApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    letter = params[:guess].to_s[0]
+    letter = params[:guess].to_s[0] || HangpersonGame.guess(letter)
     ### YOUR CODE HERE ###
     redirect '/show'
   end
